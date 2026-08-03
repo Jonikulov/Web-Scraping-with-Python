32b0964ca4251a484ff3f4f21c5e8da47133456c
Javohir_Dev <javohir200211@gmail.com>
2026-08-03 10:56:10 +0500

    build wiki article crawler

--- 08_Scrapy/wikiSpider/spiders/article.py
+++ 08_Scrapy/wikiSpider/spiders/article.py
@@ -1,23 +1,21 @@
 import scrapy
 
 class ArticleSpider(scrapy.Spider):
-    name = "article"
-    allowed_domains = ["en.wikipedia.org"]
+    name="article"
+    # allowed_domains = ["en.wikipedia.org"]
 
-    def start_requests(self):
-        # If this doesn't print, Scrapy is NOT running this file.
-        print("\n" + "="*50)
-        print("🚨 START_REQUESTS IS ACTUALLY RUNNING! 🚨")
-        print("="*50 + "\n")
-        
-        urls = ["https://en.wikipedia.org/wiki/Programming_language"]
+    async def start(self):
+        urls = [
+            "https://en.wikipedia.org/wiki/Python_(programming_language)",
+            "https://en.wikipedia.org/wiki/Functional_programming",
+            "https://en.wikipedia.org/wiki/Monty_Python",
+        ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print("\n" + "="*50)
-        print(f"✅ SUCCESS! REACHED PARSE METHOD: {response.url}")
-        print("="*50 + "\n")
-        
-        title = response.css("h1::text").get()
+        title = response.css("h1 ::text").extract_first()
+        print(f"URL is: {response.url}")
+        print(f"Title is: {title}")
+
         yield {"title": title, "url": response.url}
